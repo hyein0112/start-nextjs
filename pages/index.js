bce87984d664c795ef114acf558fa5865ecb0a8a
hyein0112 <bhi12134@gmail.com>
2022-12-12 12:07:15 +0900

    #2.1 Fetching Data

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,31 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import Seo from "../components/Seo";
+
 export default function Home() {
+  const [movies, setMovies] = useState();
+
+  const getData = async () => {
+    await axios
+      .get(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)
+      .then((data) => {
+        console.log(data.data.results);
+        setMovies(data.data.results);
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <>
       <Seo title="Home" />
-      <h1>Hello</h1>
+      {!movies && <h1>Loading...</h1>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </>
   );
 }
