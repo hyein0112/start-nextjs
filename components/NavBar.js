ff573c47702852753b10a1b9e25656932db2fced
hyein0112 <bhi12134@gmail.com>
2022-12-12 15:53:35 +0900

    #2.2 Redirect and Rewrite

--- components/NavBar.js
+++ components/NavBar.js
@@ -6,26 +6,32 @@ export default function NavBar() {
   return (
     <nav>
       <img src="/vercel.svg" />
-      <Link href="/">
-        <a className={router.pathname === "/" && "active"}>Home</a>
-      </Link>
-      <Link href="/about">
-        <a className={router.pathname === "/about" && "active"}>About</a>
-      </Link>
+      <div>
+        <Link href="/">
+          <a className={router.pathname === "/" && "active"}>Home</a>
+        </Link>
+        <Link href="/about">
+          <a className={router.pathname === "/about" && "active"}>About</a>
+        </Link>
+      </div>
       <style jsx>{`
         nav {
           display: flex;
           gap: 10px;
           flex-direction: column;
           align-items: center;
-          padding-top: 20px;
-          padding-bottom: 10px;
+          padding: 2vw;
+          margin-bottom: 20px;
+          box-sizing: border-box;
           box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
             rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
         }
         img {
           max-width: 100px;
           margin-bottom: 5px;
+        }
+        nav div {
+          display: flex;
         }
         nav a {
           font-weight: 600;
--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,10 @@ export default function Home() {
   const [movies, setMovies] = useState();
 
   const getData = async () => {
-    await axios
-      .get(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)
-      .then((data) => {
-        console.log(data.data.results);
-        setMovies(data.data.results);
-      });
+    await axios.get(`/api/movies`).then((data) => {
+      console.log(data.data.results);
+      setMovies(data.data.results);
+    });
   };
 
   useEffect(() => {
@@ -21,11 +19,58 @@ export default function Home() {
     <>
       <Seo title="Home" />
       {!movies && <h1>Loading...</h1>}
-      {movies?.map((movie) => (
-        <div key={movie.id}>
-          <h4>{movie.original_title}</h4>
-        </div>
-      ))}
+      <div className="container">
+        {movies?.map((movie) => (
+          <div className="movie" key={movie.id}>
+            <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+            <h4>{movie.original_title}</h4>
+          </div>
+        ))}
+      </div>
+      <style jsx>{`
+        h1 {
+          display: block;
+          text-align: center;
+        }
+        .container {
+          width: 100%;
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 10px;
+          gap: 20px;
+        }
+        .movie {
+          display: flex;
+          flex-direction: column;
+          padding: 15px;
+          box-shadow: rgba(0, 0, 0, 0.5) 0px 4px 12px;
+          background: #ffffff;
+          transition: transform 0.2s ease-in-out;
+          box-sizing: border-box;
+        }
+        .movie:hover {
+          transform: scale(1.04);
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+        @media only screen and (max-width: 510px) {
+          .movie h4 {
+            font-size: 14px;
+          }
+        }
+        @media only screen and (max-width: 380px) {
+          .movie h4 {
+            font-size: 10px;
+          }
+        }
+      `}</style>
     </>
   );
 }
